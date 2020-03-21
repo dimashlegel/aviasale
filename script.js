652f50c5a7895590f9be3938ad858f11f9e72f99
Dima <dima@simply-smart.com>
2020-03-21 18:56:19 +0200

    connect database and XMLHttpRequest

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// DOM Elems
 const formSearch = document.querySelector('.form-search'),
 	inputCitiesFrom = document.querySelector('.input__cities-from'),
 	dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
@@ -5,26 +6,52 @@ const formSearch = document.querySelector('.form-search'),
 	dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
 	inputDateDepart = document.querySelector('.input__date-depart');
 
-const city = ["Київ", "Львів", "Дніпро", "Ізяслав", "Кременчук", "Бат-Ям", "Тель-Авів", "Єрусалим", "Хмельницький", "Одеса"];
 
+// data
+const CITIES_API = 'data/cities.json',
+	PROXY = 'https://cors-anywhere.herokuapp.com/',
+	API_HEY = 'be0543f1095ab466ccebdbe319b11518',
+	CALENDAR = 'http://min-prices.aviasales.ru/calendar_preload?currency=usd&origin=IEV&destination=TLV&depart_date=2020-06-04&return_date=2020-06-14&one_way=false';
+
+let city = [];
+
+const getData = (url, callback) => {
+	const request = new XMLHttpRequest();
+
+	request.open('GET', url);
+
+	request.addEventListener('readystatechange', () => {
+		// status 4 - eto otvet ot servera
+		if (request.readyState !== 4) return;
+		if (request.status === 200) {
+			callback(request.response);
+		} else {
+			console.error(request.status);
+		};
+	})
+
+	request.send();
+}
+
+// show CityList
 const showCity = (input, list) => {
-	const filterCity = city.filter((item) => {
-		list.innerHTML = '';
-		if (input.value !== '') {
-			return item.toLowerCase().includes(input.value.toLowerCase());
-		}
-		return false;
-	})
-
-	filterCity.forEach((item) => {
-		const li = document.createElement('li');
-		li.classList.add('dropdown__city');
-		li.textContent = item;
-		list.append(li);
-	})
+	list.innerHTML = '';
+	if (input.value !== '') {
+		const filterCity = city.filter((item) => {
+			return item.name.toLowerCase().includes(input.value.toLowerCase());
+		})
+		filterCity.forEach((item) => {
+			const li = document.createElement('li');
+			li.classList.add('dropdown__city');
+			li.textContent = item.name;
+			list.append(li);
+		})
+	}
 };
 
-function markCity(event, field, parent) {
+
+// mark City
+const handlerCity = (event, field, parent) => {
 	const target = event.target;
 	if (target.tagName.toLowerCase() === "li") {
 		field.value = target.textContent;
@@ -32,19 +59,31 @@ function markCity(event, field, parent) {
 	}
 }
 
+
 // list cities from
 inputCitiesFrom.addEventListener('input', () => {
 	showCity(inputCitiesFrom, dropdownCitiesFrom);
 });
 dropdownCitiesFrom.addEventListener('click', (e) => {
-	markCity(e, inputCitiesFrom, dropdownCitiesFrom);
+	handlerCity(e, inputCitiesFrom, dropdownCitiesFrom);
 });
+
 
 // list cities to
 inputCitiesTo.addEventListener('input', () => {
 	showCity(inputCitiesTo, dropdownCitiesTo);
 });
 dropdownCitiesTo.addEventListener('click', (e) => {
-	markCity(e, inputCitiesTo, dropdownCitiesTo);
+	handlerCity(e, inputCitiesTo, dropdownCitiesTo);
 });
 
+
+// functions calls
+getData(CITIES_API, (data) => {
+	city = JSON.parse(data).filter(item => item.name);
+});
+
+getData(PROXY + CALENDAR, (data) => {
+	reys = JSON.parse(data);
+	console.log(reys);
+});
